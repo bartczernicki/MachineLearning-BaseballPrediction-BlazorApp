5e17b1cc33e2fe53824ad1c22522c9462a057604
Bart Czernicki <bartczernicki@outlook.com>
2019-09-22 17:59:57 +0000

    Refactoring to components.

---
+++ BaseballPredictionBlazor.Shared/MLModelPredictionType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballPredictionBlazor.Shared
+{
+    public static class MLModelPredictionType
+    {
+        /// <summary>
+        /// Predict the probability of the baseball player being on the BALLOT of the Hall of Fame.
+        /// </summary>
+        public const string OnHallOfFameBallot = "OnHallOfFameBallot";
+
+        /// <summary>
+        /// Predict the probability of the baseball player being INDUCTED to the Hall of Fame.
+        /// </summary>
+        public const string InductedToHallOfFame = "InductedToHallOfFame";
+
+        public static string GetPredictionLabel(string mlModelPredictionType)
+        {
+            var predictionLabel = string.Empty;
+
+            if (mlModelPredictionType == OnHallOfFameBallot)
+            {
+                predictionLabel = "On Hall of Fame Ballot";
+            }
+            else
+            {
+                predictionLabel = "Hall Of Fame Induction";
+            }
+
+            return predictionLabel;
+        }
+    }
+}
