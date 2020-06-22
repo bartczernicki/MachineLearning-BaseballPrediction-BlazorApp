a24d5d95dbb0fa0be31e2ecf6657f5708d11988c
Bart Czernicki <bartczernicki@outlook.com>
2020-06-22 15:21:22 +0000

    Updated JS references and SignalR connection; added Logging. Updated favicon. Added animated images for ShowMe.

--- BaseballMachineLearningWorkbench/Startup.cs
+++ BaseballMachineLearningWorkbench/Startup.cs
@@ -63,6 +63,7 @@ namespace BaseballMachineLearningWorkbench
             services.AddPredictionEnginePool<MLBBaseballBatter, MLBHOFPrediction>()
                 .FromFile("InductedToHallOfFameGeneralizedAdditiveModel", modelPathInductedToHallOfFameGeneralizedAdditiveModel)
                 .FromFile("OnHallOfFameBallotGeneralizedAdditiveModel", modelPathOnHallOfFameBallotGeneralizedAdditiveModel);
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
