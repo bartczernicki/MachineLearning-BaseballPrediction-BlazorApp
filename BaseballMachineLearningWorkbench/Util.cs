ebcd1ab81197b5f6d22210494aac43e89cc04baf
Bart Czernicki <bartczernicki@outlook.com>
2020-08-29 00:37:36 +0000

    Adding Routing Parameters to have permanent URLs.

---
+++ BaseballMachineLearningWorkbench/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseballMachineLearningWorkbench
+{
+    public static class Util
+    {
+        public static string RemoveWhiteSpace(string stringWithPotentialWhiteSpace)
+        {
+            var whiteSpeaceRemoved = String.Concat(stringWithPotentialWhiteSpace.Where(c => !Char.IsWhiteSpace(c)));
+
+            return whiteSpeaceRemoved;
+        }
+    }
+}
